142534a33d384b2681d554d19a679568ed238067
MLoretto <loreto.henriquez.m@gmail.com>
2018-09-23 18:49:52 -0300

    Cambios generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import { BtnGoogleComponent } from './buttons/btn-google/btn-google.component';
 import { BtnReturnComponent } from './buttons/btn-return/btn-return.component';
 import { InfoProfileComponent } from './graf/info-profile/info-profile.component';
 import { FormEditProfileComponent } from './forms/form-edit-profile/form-edit-profile.component';
-import { BackgroundWallComponent } from './graf/background-wall/background-wall.component';
 import { PostComponent } from './card/post/post.component';
 import { HeaderComponent } from './graf/header/header.component';
 
@@ -46,7 +45,6 @@ import { HeaderComponent } from './graf/header/header.component';
     InfoProfileComponent,
     FormEditProfileComponent,
     PostComponent,
-    BackgroundWallComponent,
     HeaderComponent
   ],
   imports: [
