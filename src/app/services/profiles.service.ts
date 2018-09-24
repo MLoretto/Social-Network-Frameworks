030dfdd06bd65a92df477f951e95ba994aa31fe5
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-09-23 22:33:14 -0300

    service firebase profile

--- src/app/services/profiles.service.ts
+++ src/app/services/profiles.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Action } from 'rxjs/internal/scheduler/Action';
+
+export interface ProfileData {
+  email: string; date: Date; uid: string; name: string;
+  age: number; photo: string; review: string; areasOfInterest: string[]; level: string[]
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProfilesService {
 
-  constructor() { }
+  private ProfileDataCollection: AngularFirestoreCollection<ProfileData>
+  ProfileData: Observable<ProfileData[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.ProfileDataCollection = afs.collection<ProfileData>('ProfileData');
+    this.ProfileData = this.ProfileDataCollection.valueChanges();
+  }
 
 
-  
+  addProfileData(email: string, uid: string) {
+    const data: ProfileData = {
+      email: email,
+      date: new Date(),
+      uid: uid,
+      name: null,
+      age: null,
+      photo: null,
+      review: null,
+      areasOfInterest: null,
+      level: null
+    }
+    return this.ProfileDataCollection.add(data);
+  }
+
 }
+
